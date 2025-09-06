48eb436b679381717434694ebee58c45dcc1d3e0
Kanwarpreet <KanwarpreetSidana1200@gmail.com>
2025-09-06 14:01:12 +0530

    FIrst Checkin

--- TestWebApp/Controllers/WeatherForecastController.cs
+++ TestWebApp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestWebApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Extreme"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
